8f591ad5984e4bb35e5015e45cfc30747548ca92
Rajarshi Karmakar <advancedroboticssystem@gmail.com>
2025-09-15 21:08:49 +0000

    project png add

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -27,7 +27,7 @@ const projects = [
     description: "A high performance Keypoint detection and matching model with the semantic context for SLAM in robotics, achieving state-of-the-art benchmark for cityscape.",
     technologies: ["Pytorch", "Tensorboard", "Git", "CUDA"],
     role: "AI Researcher at Valeo",
-    image: "https://private-user-images.githubusercontent.com/130791740/489702655-18cb244e-2b0d-4146-b156-0396b25e6ef7.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTc5NjQxODksIm5iZiI6MTc1Nzk2Mzg4OSwicGF0aCI6Ii8xMzA3OTE3NDAvNDg5NzAyNjU1LTE4Y2IyNDRlLTJiMGQtNDE0Ni1iMTU2LTAzOTZiMjVlNmVmNy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwOTE1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDkxNVQxOTE4MDlaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW1zLVNpZ25hdHVyZT1kZjMzNDczNjU0MmIwMDhjZGI1MTllZDk5ODI3NDc5ZDg0OWE3YWQ4YTUwMmZkNjBhMWNhMDc5MWRlYzkzZjM2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.upNqTdWUHkVlqvohx4LbMXAwZ1D_Rh_uN9LdMh4MbQE",
+    image: "/segmentation-matching.png",
     demoLink: "https://github.com/user-attachments/assets/d1b99108-9b3b-4d37-a1d5-35b6da3a99b2",
     githubLink: "https://github.com/rkarmaka98/SuperSegmentaion",
     // blogLink: "#",
