236957ffebc55f1ea4940794cb4cb8adc3e79318
Rajarshi Karmakar <advancedroboticssystem@gmail.com>
2025-09-15 19:36:08 +0000

    github pages setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
     ],
     "jobTitle": "AI Software Engineer & Researcher",
     // Replace with a real profile image
-    "image": "https://media.licdn.com/dms/image/v2/D4D03AQG4VO-QTFnCpw/profile-displayphoto-scale_400_400/B4DZh3EFasGkAg-/0/1754344217006?e=1760572800&v=beta&t=O8MNOnYfoOPtfVE2ohlzGRkhJex_o59mU6kbNA_1Mu4" 
+    "image": "/profile.jpg" 
   };
 
   return (
